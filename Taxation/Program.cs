79ef74268e553dd9235339c65179b3163ef122b0
Abdulrahman3wd <abdulrahmanawad7861991@gmail.com>
2024-06-07 04:48:00 +0300

    Taxes Task

--- Taxation/Program.cs
+++ Taxation/Program.cs
@@ -24,7 +24,7 @@ namespace Taxation
 
 			app.MapControllerRoute(
 				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+				pattern: "{controller=Home}/{action=ExcelFileReader}/{id?}");
 
 			app.Run();
 		}
